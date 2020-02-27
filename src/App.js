46f8880efb940070a99fcb5128dd005e16c681a0
ovidiumilitaru <militaru.ovidiu@gmail.com>
2020-02-28 01:35:58 +0200

    with Nagigation and Logo components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import React, { Component} from 'react';
-import Navigation from './components/Navigation/Navigation';
-import './App.css';
-
+import React, { Component } from "react";
+import Navigation from "./components/Navigation/Navigation";
+import Logo from "./components/Logo/Logo";
+import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Navigation />
-        {/* <Logo />
+        <Logo />
         <ImageLinkForm />
-        <FaceRecognition /> */}
+        {/* <FaceRecognition /> */}
       </div>
     );
   }
