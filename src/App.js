1160aecc145a3e49daf07669b3b1af6de8dc79d9
ovidiumilitaru <militaru.ovidiu@gmail.com>
2020-03-10 01:01:46 +0200

    with faceBox

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React, { Component } from "react";
-import Particles from 'react-particles-js';
-import Clarifai from 'clarifai';
+import Particles from "react-particles-js";
+import Clarifai from "clarifai";
 import Navigation from "./components/Navigation/Navigation";
+import FaceRecognition from "./components/FaceRecognition/FaceRecognition";
 import Logo from "./components/Logo/Logo";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
 import Rank from "./components/Rank/Rank";
 import "./App.css";
 
 const app = new Clarifai.App({
-  apiKey: '7eb22c6af4ab4d4da8635301e25e2f8c'
- });
+  apiKey: "7eb22c6af4ab4d4da8635301e25e2f8c"
+});
 
 const particlesOptions = {
   particles: {
@@ -21,49 +22,65 @@ const particlesOptions = {
       }
     }
   }
-}
+};
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      input: '',
-    }
+      input: "",
+      imageUrl: "",
+      box: {}
+    };
   }
 
-  onInputChange = (event) => {
-    console.log(event.target.value);
-  }
+  calculateFaceLocation = data => {
+    const clarifaiFace =
+      data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById("inputimage");
+    const width = Number(image.width);
+    const height = Number(image.height);
+    console.log(width, height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - clarifaiFace.right_col * width,
+      bottomRow: height - clarifaiFace.bottom_row * height
+    };
+  };
+
+  displayFaceBox = box => {
+    console.log(box);
+    this.setState({ box: box });
+  };
+
+  onInputChange = event => {
+    this.setState({ input: event.target.value });
+  };
 
   onButtonSubmit = () => {
-    console.log('click');
+    this.setState({ imageUrl: this.state.input });
     app.models
-      .predict(
-        "7eb22c6af4ab4d4da8635301e25e2f8c", 
-        "https://samples.clarifai.com/face-det.jpg")
-      .then(
-        function(response) {
-        console.log(response);
-        // do something with response
-      },
-      function(err) {
-        // there was an error
-      }
-    );
-  }
+      .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+      .then(response =>
+        this.displayFaceBox(this.calculateFaceLocation(response))
+      )
+      .catch(err => console.log(err));
+  };
 
   render() {
     return (
       <div className="App">
-        <Particles className='particles' params={particlesOptions} />
+        <Particles className="particles" params={particlesOptions} />
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm 
-        onInputChange={this.onInputChange} 
-        onButtonSubmit={this.onButtonSubmit}/>
+        <ImageLinkForm
+          onInputChange={this.onInputChange}
+          onButtonSubmit={this.onButtonSubmit}
+        />
 
-        {/* <FaceRecognition /> */}
+        <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl} />
       </div>
     );
   }
